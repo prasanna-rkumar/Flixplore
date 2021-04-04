c8e1c0453c1e9c1dfb5b64e11029dd3cbb6d2c8f
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-04 23:04:34 +0530

    FIX: badge radius, watch-list filter sticky

--- components/RightPane/WatchBadge.js
+++ components/RightPane/WatchBadge.js
@@ -4,7 +4,7 @@ import propTypes from 'prop-types';
 const WatchBadge = ({ movieData, error }) => {
   if (!movieData || error) return <></>;
   return (
-    <div className="bg-pink-200 rounded-sm text-pink-700 absolute top-4 right-0 font-bold text-xs px-0.5 z-10">
+    <div className="bg-pink-200 rounded-sm text-pink-700 absolute top-4 right-0 font-bold text-xs z-10 py-0.5 px-1" style={{ borderRadius: '4px 0 0 4px' }}>
       {movieData.is_seen ? 'Seen' : 'In Watch Queue'}
     </div>
   );
--- components/WatchList/FilterOptions/index.js
+++ components/WatchList/FilterOptions/index.js
@@ -4,8 +4,8 @@ import styles from './FilterOptions.module.css';
 const filterOptions = ['ALL', 'TO WATCH', 'WATCHED'];
 
 const FilterOptions = ({ filter, setFilter }) => (
-  <div className="pb-1">
-    <section className="flex flex-row gap-8 mb-4 mx-1 pb-1 border-b-2 border-gray-500 sticky bg-primary z-50" style={{ top: '4.1rem' }}>
+  <div className=" sticky bg-primary z-50 py-2" style={{ top: '4.1rem' }}>
+    <section className="flex flex-row gap-8 mx-1 pb-1 border-b-2 border-gray-500">
       {filterOptions.map((value) => (
         <div role="button" tabIndex={0} onKeyDown={() => { }} onClick={() => setFilter(value)} className={`${styles.filterOption} ${filter === value ? styles.selected : ''} text-lg relative text-white text-opacity-70 group cursor-pointer`} key={value}>
           <span className={styles.optionName}>{value}</span>
