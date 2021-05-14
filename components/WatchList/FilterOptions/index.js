176d5316d5664f5d17a990c67b071dd497b6288a
Prasanna Kumar <prasannasrk07@gmail.com>
2021-05-14 17:29:14 +0530

    💄 watch-list filter menubar zindex fix

--- components/WatchList/FilterOptions/index.js
+++ components/WatchList/FilterOptions/index.js
@@ -4,7 +4,7 @@ import styles from './FilterOptions.module.css';
 const filterOptions = ['ALL', 'TO WATCH', 'WATCHED'];
 
 const FilterOptions = ({ filter, setFilter }) => (
-  <div className=" sticky bg-primary z-50 py-2" style={{ top: '4.1rem' }}>
+  <div className=" sticky bg-primary z-20 py-2" style={{ top: '4.1rem' }}>
     <section className="flex flex-row gap-8 mx-1 pb-1 border-b-2 border-gray-500">
       {filterOptions.map((value) => (
         <div role="button" tabIndex={0} onKeyDown={() => { }} onClick={() => setFilter(value)} className={`${styles.filterOption} ${filter === value ? styles.selected : ''} text-lg relative text-white text-opacity-70 group cursor-pointer`} key={value}>
