2a88afb6ade88168ef4a026dc8e28e97c122d2e9
Prasanna Kumar <prasannasrk07@gmail.com>
2021-05-17 13:38:22 +0530

    🩹 alert when tries to share private playlists

--- pages/playlist/[playlist_name]/[playlist_id].js
+++ pages/playlist/[playlist_name]/[playlist_id].js
@@ -4,13 +4,14 @@ import { useQuery } from 'react-query';
 import Link from 'next/link';
 import { FiShare2 } from 'react-icons/fi';
 import { IoLogoTwitter } from 'react-icons/io';
+import { toast } from 'react-toastify';
 import { getPlaylistMovies } from '../../../utils/dbHelper';
 import API, { END_POINTS } from '../../../tmdb-api';
 import SEO from '../../../components/SEO';
 import serverSupabase from '../../../utils/initSecretSupabase';
 import Header from '../../../components/shared/Header';
 
-const PlaylistMovies = ({ id, playlistName }) => {
+const PlaylistMovies = ({ id, playlistName, isPublic }) => {
   const [playlistMovies, setPlaylistMovies] = useState([]);
   const [shareableURL, setURL] = useState('');
 
@@ -33,16 +34,35 @@ const PlaylistMovies = ({ id, playlistName }) => {
       <main className="relative py-4 px-3 items-start max-w-screen-2xl m-auto xl:px-16 mt-2">
         <h1 className="text-3xl text-white font-bold text-center">{playlistName}</h1>
         <div className="flex gap-3 justify-center items-center mt-2">
-          <a target="_blank" rel="noreferrer noopener" href={`https://twitter.com/intent/tweet?url=${shareableURL}&text=Check out this curated list of ${playlistName}`}>
-            <IoLogoTwitter size={24} color="#00acee" />
+          <a
+            target="_blank"
+            onClick={(e) => {
+              if (!isPublic) {
+                e.preventDefault();
+                toast.error('Cannot share your private playlists');
+              }
+            }}
+            rel="noreferrer noopener"
+            href={`https://twitter.com/intent/tweet?url=${shareableURL}&text=Check out this curated list of ${playlistName}`}
+          >
+            <IoLogoTwitter
+              size={24}
+              color="#00acee"
+            />
           </a>
           <button
             type="button"
-            onClick={() => navigator.share({
-              title: 'Flixplore',
-              text: playlistName,
-              url: shareableURL,
-            })}
+            onClick={() => {
+              if (!isPublic) {
+                toast.error('Cannot share your private playlits');
+              } else {
+                navigator.share({
+                  title: 'Flixplore',
+                  text: playlistName,
+                  url: shareableURL,
+                });
+              }
+            }}
           >
             <FiShare2 size={24} className="text-gray-200" />
           </button>
@@ -60,11 +80,13 @@ const PlaylistMovies = ({ id, playlistName }) => {
 PlaylistMovies.propTypes = {
   id: propTypes.number,
   playlistName: propTypes.string,
+  isPublic: propTypes.bool,
 };
 
 PlaylistMovies.defaultProps = {
   id: 0,
   playlistName: '',
+  isPublic: false,
 };
 
 export default PlaylistMovies;
@@ -140,6 +162,7 @@ export async function getServerSideProps({ req, res, params }) {
     props: {
       id: data.id,
       playlistName: data.playlist_name,
+      isPublic: data.is_public,
     },
   };
 }
